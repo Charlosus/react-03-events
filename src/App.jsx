2769998c162e57dde4b5bcd3ebf7297750d6789b
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-05 21:38:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  // this use of useEffect will happen in every change of state
+  // on any change of our virtual DOM efect
+  // this is realy bad effect and we sound NEVER do
+  // useEffect(() => {
+  //   console.log('on mount and every state change');
+  // });
+
+  // if we add an empty array after declaration we will cause
+  // that code in body of useEffect will ocure only once
+  // at first render of code this is the way we want to manage
+  // API asks or any getter of any dataquery
+
+  useEffect(() => {
+    console.log('on mount');
+  }, []); // this exact line that make the magic happen
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={() => setCounter((p) => p + 1)}>Click{counter}</button>
+      <button onClick={() => setCounter2((p) => p + 1)}>Click{counter2}</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
